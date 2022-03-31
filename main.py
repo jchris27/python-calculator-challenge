d010946031b1458405f120540979270041b7c199
Jin <53550425+jchris27@users.noreply.github.com>
2022-03-31 13:11:22 +0000

    added an if statement

--- main.py
+++ main.py
@@ -15,3 +15,34 @@ def multiply(n1, n2):
 # Divide
 def divide(n1, n2):
   return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for symbol in operations:
+  operation = ""
+  operation += symbol
+  print(operation)
+operation_symbol = input("Pick an operation from the line above: ")
+
+answer = 0
+
+if operation_symbol == "+":
+  answer = add(num1, num2)
+elif operation_symbol == "-":
+  answer = subtract(num1, num2)
+elif operation_symbol == "*":
+  answer = multiply(num1, num2)
+elif operation_symbol == "/":
+  answer = divide(num1, num2)
+else:
+  print("Invalid operation symbol. Try again")
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
